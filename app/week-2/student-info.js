08b59466d6704be68d49fd8294c260206af3ab21
Susan Steckle <susan.steckle@edu.sait.ca>
2025-09-13 20:50:02 -0600

    Updates StudentInfo to li items, adds Link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Sue Steckle</h1>
+      <ul>
+        <li>Sue Steckle</li>
+        <li>
+          <Link
+            target="_blank"
+            href="https://github.com/SueAtSAIT/cprg306-assignments">
+            My SueAtSAIT GitHub Repo for cprg306 Assignments
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
