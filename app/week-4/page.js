c29584b726334097b4c2234e75308672ca602960
Susan Steckle <susan.steckle@edu.sait.ca>
2025-09-28 21:24:49 -0600

    Adds week-4 files and first couple lines of code only.

---
+++ app/week-4/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <>
+      <header>
+        <h1>Build Your List...</h1>
+        <p>Use this page to add items to your Shopping List.</p>
+      </header>
+    </>
+  );
+}
