4185518fd17ec58154e27d867cee36e9b8760839
Susan Steckle <susan.steckle@edu.sait.ca>
2025-10-01 15:29:48 -0600

    Updates footer element to contain student-info plus a bit more formatting

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,20 +2,18 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <main>
-      <ul>
-        <li className="text-purple-500 hover:text-purple-700">
-          Sue Steckle (00987166)
-        </li>
-        <li>
-          <Link
-            className="hover:font-semibold"
-            target="_blank"
-            href="https://github.com/SueAtSAIT/cprg306-assignments">
-            My SueAtSAIT GitHub Repo for cprg306 Assignments
-          </Link>
-        </li>
-      </ul>
-    </main>
+    <ul>
+      <li className="text-purple-500 hover:text-purple-700">
+        Sue Steckle (00987166)
+      </li>
+      <li className="mb-3">
+        <Link
+          className="hover:font-semibold"
+          target="_blank"
+          href="https://github.com/SueAtSAIT/cprg306-assignments">
+          My SueAtSAIT GitHub Repo for cprg306 Assignments
+        </Link>
+      </li>
+    </ul>
   );
 }
--- app/week-3/footer.js
+++ app/week-3/footer.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import StudentInfo from "../week-2/student-info";
 
 export default function FooterLink() {
   return (
     <footer className="text-center my-10 font-bold">
+      <StudentInfo />
       <Link href="../.">Back to home page...</Link>
     </footer>
   );
