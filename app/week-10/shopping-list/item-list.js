f21bca3765f7c70745512fec62b18d3cdfe073b2
Susan Steckle <susan.steckle@edu.sait.ca>
2025-11-24 22:16:54 -0700

    REFACTOR: update item-list based on week-6 critiques (old code is commented)

--- app/week-10/shopping-list/item-list.js
+++ app/week-10/shopping-list/item-list.js
@@ -1,40 +1,51 @@
 "use client";
 
 import { useState } from "react";
-
 import Item from "./item";
 import GroupedItem from "./groupeditem";
 
 export default function ItemList({ items = [], onItemSelect }) {
-  const [sortBy, setSortBy] = useState("name");
-  const [isNameActive, setIsNameActive] = useState(true);
-  const [isGroupbyActive, setIsGroupbyActive] = useState(false);
+  // const [sortBy, setSortBy] = useState("name");
+  // const [isNameActive, setIsNameActive] = useState(true);
+  // const [isGroupbyActive, setIsGroupbyActive] = useState(false);
 
-  const sortByCategory = () => {
-    setSortBy("category");
-    setIsNameActive(false);
-    setIsGroupbyActive(false);
-  };
-  const sortByName = () => {
-    setSortBy("name");
-    setIsNameActive(true);
-    setIsGroupbyActive(false);
-  };
-  const groupByCategory = () => {
-    setIsGroupbyActive(true);
-  };
+  const [view, setView] = useState("name");
 
-  const sortedList = [...items].sort((a, b) => {
-    if (sortBy === "name") {
-      return a.name.localeCompare(b.name);
-    } else if (sortBy === "category") {
-      return a.category.localeCompare(b.category);
-    } else return 0;
-  });
+  const sortByCategory = () => setView("category");
+  // {
+  //   setSortBy("category");
+  //   setIsNameActive(false);
+  //   setIsGroupbyActive(false);
+  // };
+  const sortByName = () => setView("name");
+  // {
+  //   setSortBy("name");
+  //   setIsNameActive(true);
+  //   setIsGroupbyActive(false);
+  // };
+  const groupByCategory = () => setView("group");
+  // {
+  //   setIsGroupbyActive(true);
+  // };
+
+  const sortedList =
+    view === "group"
+      ? []
+      : [...items].sort((a, b) => a[view].localeCompare(b[view]));
+
+  // {
+  //   if (sortBy === "name") {
+  //     return a.name.localeCompare(b.name);
+  //   } else if (sortBy === "category") {
+  //     return a.category.localeCompare(b.category);
+  //   } else return 0;
+  // });
 
   const groupedList = items.reduce((groupedItems, item) => {
     const category = item.category;
-    if (groupedItems[category] == null) groupedItems[category] = [];
+    if (!groupedItems[category]) {
+      groupedItems[category] = [];
+    }
     groupedItems[category].push(item);
     groupedItems[category].sort((a, b) => a.name.localeCompare(b.name));
     return groupedItems;
@@ -47,19 +58,20 @@ export default function ItemList({ items = [], onItemSelect }) {
         <button
           onClick={sortByName}
           className={`${
-            isNameActive && !isGroupbyActive
+            view === "name"
               ? "bg-violet-600 text-white"
               : "bg-white border border-gray-600 text-gray-600"
           }
             " my-6 mx-2 p-3 rounded-2xl  hover:bg-violet-400 hover:text-white active:bg-violet-900 `}>
           Name
         </button>
+
         <button
           onClick={sortByCategory}
           className={`${
-            isNameActive || isGroupbyActive
-              ? "bg-white border border-gray-600 text-gray-600"
-              : "bg-violet-600 text-white"
+            view === "category"
+              ? "bg-violet-600 text-white"
+              : "bg-white border border-gray-600 text-gray-600"
           } my-6 mx-2 p-3 rounded-2xl  hover:bg-violet-400 hover:text-white active:bg-violet-900 `}>
           Category
         </button>
@@ -67,14 +79,14 @@ export default function ItemList({ items = [], onItemSelect }) {
         <button
           onClick={groupByCategory}
           className={`${
-            isGroupbyActive
+            view === "group"
               ? "bg-violet-600 text-white"
               : "bg-white border border-gray-600 text-gray-600"
           } my-6 mx-2 p-3 rounded-2xl  hover:bg-violet-400 hover:text-white active:bg-violet-900 `}>
           Group by Category
         </button>
       </div>
-      <div className={`${isGroupbyActive ? "hidden" : ""}`}>
+      <div className={`${view === "group" ? "hidden" : ""}`}>
         <p className="ml-4">
           Click any <span className="italic">edible</span> item to search meal
           ideas...
@@ -83,7 +95,7 @@ export default function ItemList({ items = [], onItemSelect }) {
           <Item key={item.id} {...item} onSelect={onItemSelect} />
         ))}
       </div>
-      <div className={`${isGroupbyActive ? "" : "hidden"}`}>
+      <div className={`${view === "group" ? "" : "hidden"}`}>
         <GroupedItem groupedList={groupedList} />
       </div>
     </div>
